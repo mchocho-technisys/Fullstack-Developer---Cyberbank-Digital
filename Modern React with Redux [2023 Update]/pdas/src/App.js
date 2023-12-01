c782659c7e500312e9c3d2a0fe2df44cfb2683ae
mchocho-technisys <mchocho@technisys.com>
2023-11-30 21:42:35 -0300

    Adding styles with bulma css

--- Modern React with Redux [2023 Update]/pdas/src/App.js
+++ Modern React with Redux [2023 Update]/pdas/src/App.js
@@ -2,15 +2,32 @@ import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
+import 'bulma/css/bulma.css';
 
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistans</div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistans</p>
+                </div>
+            </section>
 
-            <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImage} />
-            <ProfileCard title='Cortana' handle='@cortana32' image={CortanaImage} />
-            <ProfileCard title='Siri' handle='@siri01' image={SiriImage} />
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImage} description="Alexa was createad by Amazon"/>
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title='Cortana' handle='@cortana32' image={CortanaImage} description="Cortana was createad by Microsoft" />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title='Siri' handle='@siri01' image={SiriImage} description="Siri was createad by Apple" />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
